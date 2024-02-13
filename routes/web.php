2bd3a54b9e170f45b4fed812d5b032934193b6f5
Vega <vegafernando881@gmail.com>
2024-02-13 13:01:36 -0600

    Configuracion de Route::resource

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::resource('cursos', CursoController::class);
