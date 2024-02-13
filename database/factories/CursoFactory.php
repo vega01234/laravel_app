5404f5b3551e5fe9545cf603bf1e2e3cfd41d3b7
Vega <vegafernando881@gmail.com>
2024-02-13 13:01:10 -0600

    Creacion de Factory y DB para Curso

---
+++ database/factories/CursoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CursoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->randomElement(['El Infierno de Java', 'Python Basico', 'Introduccion a Laravel', 'PHP Intermedio', 'C++ La Venganza']),
+            'description' => $this->faker->text(),
+            'category' => $this->faker->randomElement(['Front End', 'Back End'])
+        ];
+    }
+}
